3a94f580fb59c2ded6b13e7085c82b369425701a
Diwas Joshi <dj.dij123@gmail.com>
2017-10-17 13:32:01 +0530

    user model made

--- models/user.js
+++ models/user.js
@@ -1,7 +1,44 @@
 var mongoose = require('mongoose');
-var brcypt = require('bcrypt-js');
+var bcrypt = require('bcrypt-nodejs');
+var crypto = require('crypto');
+var Schema = mongoose.Schema;
 
-var userSchema = new mongoose.Schema({
-    email: { type: String, unique: true, lowercase: true},
+/* The user schema attributes / characteristics / fields */
+var UserSchema = new Schema({
 
-})
+  email: { type: String, unique: true, lowercase: true},
+  password: String,
+
+  profile: {
+    name: { type: String, default: ''},
+    picture: { type: String, default: ''}
+  },
+
+  address: String,
+  history: [{
+    date: Date,
+    paid: { type: Number, default: 0},
+  }]
+});
+
+
+/* create user */
+UserSchema.methods.createUser = function(newUser, callback){
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(newUser.password, salt, null, function(err, hash) {
+        if (err) return next(err);
+        newUser.password = hash;
+        newUser.save(callback);
+      });
+    });
+}
+
+
+/* compare password in the database and the one that the user type in */
+UserSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
+
+module.exports = mongoose.model('User', UserSchema);
